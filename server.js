6e63a79bb9a0a13a9bef3a660681a98c8bf21f45
Parth731 <patelparth31795@gmail.com>
2021-03-30 23:34:58 +0530

    part-5 commit

--- server.js
+++ server.js
@@ -2,22 +2,31 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path');
-const expressLayout = require('express-ejs-layouts');
 const port = process.env.PORT || 8000;
-
 const view_path = path.join(__dirname, './resources/views');
 
 app.use(express.static('public'));
+
+/* set the template engine */
+app.set('view engine', 'ejs');
+app.set('views', view_path);
 
 app.get('/', (req, res) => {
    res.render('home');
    // res.end();
 });
 
-/* set the template engine */
-app.use(expressLayout);
-app.set('views', view_path);
-app.set('view engine', 'ejs');
+app.get('/cart', (req, res) => {
+   res.render('customers/cart');
+});
+
+app.get('/login', (req, res) => {
+   res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+   res.render('auth/register');
+});
 
 app.listen(port, (e) => {
    console.log(`listning on port ${port}`);
