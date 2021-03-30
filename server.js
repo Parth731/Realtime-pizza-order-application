bf699796a24ace4197b42b9f752b0cacc2526faa
Parth731 <patelparth31795@gmail.com>
2021-03-30 18:55:24 +0530

    part - 4 commit

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path');
-const expressLayout = require("express-ejs-layouts");
+const expressLayout = require('express-ejs-layouts');
 const port = process.env.PORT || 8000;
-
 
 const view_path = path.join(__dirname, './resources/views');
 
+app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.render('home');
-    // res.end();
+app.get('/', (req, res) => {
+   res.render('home');
+   // res.end();
 });
-
 
 /* set the template engine */
 app.use(expressLayout);
 app.set('views', view_path);
 app.set('view engine', 'ejs');
 
-
 app.listen(port, (e) => {
-    console.log(`listning on port ${port}`)
+   console.log(`listning on port ${port}`);
 });
---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js/app.js').sass(
+   'resources/scss/app.scss',
+   'public/CSS/app.css',
+);
+
+mix.autoload({
+   jquery: ['$', 'window.jQuery'],
+});
